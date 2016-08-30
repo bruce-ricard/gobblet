02958eb1c094a1d336539420698e4e8423cb66c9
Bruce Ricard <bruce.ricard@gmail.com>
2016-08-30 03:28:46 -0400

    finish users

--- lib/users.ml
+++ lib/users.ml
@@ -9,14 +9,14 @@ type registration_result =
 module type USERS =
   sig
     val register : user -> password -> registration_result
-    val check_password : user -> password -> bool
+    val get_user : user -> password -> User.user option
   end
 
 module type USER_DB =
   sig
-    type t
-    val get : user -> password option
-    val put : user -> password -> unit
+    val get : user -> password -> User.user option
+    val exists : user -> bool
+    val put : user -> User.user  -> unit
   end
 
 module Users(User_db : USER_DB) : USERS =
@@ -25,14 +25,12 @@ module Users(User_db : USER_DB) : USERS =
 
     let register user password =
       let user = normalize user in
-      match User_db.get user with
-        Some _ -> UserAlreadyExists
-      | None -> User_db.put user password; Success
+      match User_db.exists user with
+        true -> UserAlreadyExists
+      | false -> User_db.put user (new User.user user password); Success
 
-    let check_password user password =
-      match User_db.get user with
-      | None -> false
-      | Some pass -> password = pass
+    let get_user  user password =
+      User_db.get user password
   end
 
 
@@ -40,15 +38,19 @@ module Test_db : USER_DB =
   struct
     type t = (user * password) list ref
 
-    let users = ref [("bruce", "123"); ("bruce2", "123"); ("bruce3", "123"); ("arthur", "aeer")]
+    let users = [("bruce", "123"); ("bruce2", "123"); ("bruce3", "123"); ("arthur", "aeer")]
+    let users_o = ref ((List.map (fun (name, passwd) -> name, new User.user name passwd)) users)
 
-    let get user =
+    let exists login =
+      List.exists (fun (l,_) -> l = login) !users_o
+
+    let get user password =
       try
-        Some (List.assoc user !users)
+        Some (List.assoc user !users_o)
       with
         Not_found -> None
 
-    let put user password = users := (user, password) :: !users
+    let put login user = users_o := (login, user) :: !users_o
   end
 
 module Users_test = Users(Test_db)
