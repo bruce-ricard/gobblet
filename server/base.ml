f3218b6c8f9c639d1397d6f19e3e1abaf1b38cc1
Bruce Ricard <bruce.ricard@liveramp.com>
2016-12-18 17:06:39 -0800

    add message to skeleton

--- server/base.ml
+++ server/base.ml
@@ -2,51 +2,57 @@ open Eliom_content.Html5.D
 
 let _ = let open Common in let open Connection_code in ()
 
-let header_login connection_box disconnect_box () =
+let header_login () =
   let%lwt current_user = Eliom_reference.get Common.current_user in
   match current_user with
   | Some (name, _) -> Lwt.return (
                      div
                        [
                          pcdata ("Logged in as " ^ name);
-                         disconnect_box ()
+                         Connection_code.disconnect_box ()
                        ]
                    )
-  | None -> connection_box ()
+  | None -> Connection_code.connection_box ()
 
-let header connection_box disconnect_box main_service show_games () =
-  let%lwt login = header_login connection_box disconnect_box () in
+let header () =
+  let%lwt login = header_login () in
   let menu =
     div ~a:[a_class ["menu"]]
         [
-          a show_games [pcdata "Tic Tac Toe"] ()
+          a Services.show_my_games_service [pcdata "Tic Tac Toe"] ()
         ]
   in
-  Lwt.return (
-      div
-        [
-          div ~a:[a_class ["header"]]
+  let contents =
+    [
+      div ~a:[a_class ["header"]]
+          [
+            a Services.main_service
               [
-                a main_service
-                  [
-                    div ~a:[a_class ["logo"]] [
-                          div ~a:[a_class ["logo_image"]] [pcdata "put logo here"];
-                          div ~a:[a_class ["site_name"]] [pcdata "Online board games"]
-                        ];
-                  ] ();
-                div ~a:[a_class ["header_login"]] [login]
-              ];
-          menu
-        ]
+                div ~a:[a_class ["logo"]] [
+                      div ~a:[a_class ["logo_image"]] [pcdata "put logo here"];
+                      div ~a:[a_class ["site_name"]] [pcdata "Online board games"]
+                    ];
+              ] ();
+            div ~a:[a_class ["header_login"]] [login]
+          ];
+      menu
+    ] in
+  let%lwt message_eref = Eliom_reference.get Common.message_next_page in
+  let contents = match message_eref with
+      None -> contents
+    | Some message ->
+       begin
+         Eliom_reference.set Common.message_next_page None;
+         contents @ [br(); pcdata message]
+       end
+  in
+  Lwt.return (
+      div contents
     )
 
 let skeleton ?css:(css=[["css"; "TicTacToe.css"]]) ~title content =
   let%lwt header =
     header
-      Connection_code.connection_box
-      Connection_code.disconnect_box
-      Services.main_service
-      Services.show_my_games_service
       () in
   Lwt.return
     (html
--- server/common.ml
+++ server/common.ml
@@ -2,3 +2,8 @@ let current_user =
   Eliom_reference.eref
     ~scope:Eliom_common.default_session_scope
     (None : (string * User.user) option)
+
+let message_next_page =
+  Eliom_reference.eref
+    ~scope:Eliom_common.default_session_scope
+    (None : string option)
