1bd6899604e0575c84c3075fe976734b0da707d8
Bruce Ricard <bruce.ricard@liveramp.com>
2016-12-23 22:33:46 -0800

    improve install

--- install.sh
+++ install.sh
@@ -2,15 +2,18 @@ sudo apt-get update
 yes | sudo apt-get install opam
 yes | opam init
 eval `opam config env`
+eval `opam config env`
 
 opam switch 4.02.3
 eval `opam config env`
+eval `opam config env`
+opam install depext
 yes | opam depext conf-gmp.1 && yes | opam depext conf-m4.1 && yes | opam depext conf-pkg-config.1.0
 yes | opam depext conf-libpcre.1 && yes | opam depext conf-openssl.1 && yes | opam depext conf-zlib.1 && yes | opam depext dbm.1.0
 yes | opam install eliom.5.0.0
 yes | opam install extlib
 
-cd gobblet/lib
+cd lib
 oasis setup
 make
 sudo make install
