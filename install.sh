f2ab78c47a1445dea2889dd52f24438e3cc7a8ab
Bruce Ricard <bruce.ricard@liveramp.com>
2016-12-26 13:10:28 -0800

    change yes to -y

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 sudo apt-get update
 yes | sudo apt-get install opam
-yes | opam init
+opam init -y
 eval `opam config env`
 eval `opam config env`
 
 opam switch 4.02.3
 eval `opam config env`
 eval `opam config env`
-opam install depext
-yes | opam depext conf-gmp.1 && yes | opam depext conf-m4.1 && yes | opam depext conf-pkg-config.1.0
-yes | opam depext conf-libpcre.1 && yes | opam depext conf-openssl.1 && yes | opam depext conf-zlib.1 && yes | opam depext dbm.1.0
-yes | opam install eliom.5.0.0
-yes | opam install extlib
+opam install depext -y
+opam depext conf-gmp.1 -y && opam depext conf-m4.1 -y && opam depext conf-pkg-config.1.0 -y
+opam depext conf-libpcre.1 -y && opam depext conf-openssl.1 -y && opam depext conf-zlib.1 -y && opam depext dbm.1.0 -y
+opam install eliom.5.0.0
+opam install extlib
 
 cd lib
 oasis setup
