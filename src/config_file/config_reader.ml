184748262ce8590cd403fe31f59b72f705e59c5b
Bruce Ricard <bruce.ricard@gmail.com>
2018-02-02 02:45:56 +0000

    add log level to config

--- src/config_file/config_reader.ml
+++ src/config_file/config_reader.ml
@@ -5,11 +5,20 @@ let group = new group
 let string_option_cp = new option_cp string_wrappers
 let int_option_cp = new option_cp int_wrappers
 
-let socp p = string_option_cp ~group (["postgres"] @ [p]) None
-let iocp p = int_option_cp ~group (["postgres"] @ [p]) None
+module ServerConfig : sig val log_level : string_cp end=
+  struct
+    let log_level =
+      new string_cp ~group ["server"; "log_level"] "INFO" ""
+  end
 
-let host = socp "host" "Postgres host"
-let port = iocp "port" "Postgres port"
-let database = socp "database" "Postgres database"
-let user = socp "user" "Postgres user"
-let password = socp "password" "Postgres password"
+module PostgresConfig =
+  struct
+    let socp p = string_option_cp ~group (["postgres"] @ [p]) None
+    let iocp p = int_option_cp ~group (["postgres"] @ [p]) None
+
+    let host = socp "host" "Postgres host"
+    let port = iocp "port" "Postgres port"
+    let database = socp "database" "Postgres database"
+    let user = socp "user" "Postgres user"
+    let password = socp "password" "Postgres password"
+  end
--- src/server/config_parser.ml
+++ src/server/config_parser.ml
@@ -1,8 +1,20 @@
 let () = Config_reader.group#read "config/config"
 
+module ServerConfig =
+  struct
+    open Config_reader.ServerConfig
+    let log_level =
+      match log_level#get with
+      | "ERROR" -> Logs.Error
+      | "WARNING" -> Logs.Warning
+      | "INFO" -> Logs.Info
+      | "DEBUG" -> Logs.Debug
+      | s -> failwith (Printf.sprintf "Invalid log level '%s'" s)
+  end
+
 module PostgresConfig : PostgresConfiguration.CONFIG =
   struct
-    open Config_reader
+    open Config_reader.PostgresConfig
     let host = host#get
     let port = port#get
     let user = user#get
