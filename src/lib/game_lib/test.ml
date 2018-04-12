880249f6deb2bc0fa6100a906652b12a3bf97fa7
Bruce Ricard <bruce.ricard@gmail.com>
2018-04-11 23:15:44 -0700

    Started using OUnit in game_lib
    [#156705351]

--- src/lib/game_lib/test.ml
+++ src/lib/game_lib/test.ml
@@ -1,10 +1,45 @@
+open OUnit2
 open Ttt_game_lib_games
+open Ttt_game_lib_types
 
-let main () =
-  let board = XOWinBoard.empty_board () in
+let test_place_a_piece _ =
+  let board = Boards.XOWinBoard.empty_board () in
   let x : Ttt_game_lib_pieces.XOPiece.t = `X in
-  match XOWinBoard.move board ~row:1 ~column:1 x with
-    `Ok _ -> ()
-  | _ -> assert false
+  assert_equal
+    (`Ok `KeepPlaying)
+    (Boards.XOWinBoard.place board {row=1;column=1} x)
 
-let () = main ()
+let test_invalid_move _ =
+  let board = Boards.XOWinBoard.empty_board () in
+  let place = Boards.XOWinBoard.place board in
+  assert_equal
+    (`Ok `KeepPlaying)
+    (place {row = 0; column = 0} `X);
+  assert_equal
+    (`Invalid `InvalidMove)
+    (place {row = 0; column = 0} `X)
+
+let test_win _ =
+  let board = Boards.XOWinBoard.empty_board () in
+  let place = Boards.XOWinBoard.place board in
+  assert_equal
+    (`Ok `KeepPlaying)
+    (place {row = 0; column = 0} `X);
+  assert_equal
+    (`Ok `KeepPlaying)
+    (place {row = 0; column = 1} `X);
+  assert_equal
+    (`Ok `Won)
+    (place {row = 0; column = 2} `X)
+
+
+let suite =
+  "game_lib" >:::
+    [
+      "place a piece" >:: test_place_a_piece;
+      "test invalid move" >:: test_invalid_move;
+      "win test" >:: test_win;
+    ]
+
+let () =
+  run_test_tt_main suite
