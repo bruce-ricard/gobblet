a660503f9f5970503fb1db60ce98b5865e8f3f34
Bruce Ricard <bruce.ricard@gmail.com>
2018-04-18 23:24:34 -0700

    Rename test files so that they don't generate the same interface.

--- src/lib/server_lib/challenge_test.ml
+++ src/lib/server_lib/challenge_test.ml
@@ -1,30 +1,9 @@
-open OUnit2
 open Lwt
 open Ttt_common_lib_types
+open Test
 
 module CCS =
   Ttt_server_lib_challengeCriticalSection.Make(List_challenge_store)
 
 let challenge =
   Ttt_server_lib_challenge.create "bob" (new id 42)
-
-let create_challenge _ =
-  let cs = CCS.create () in
-
-  assert_equal [] (CCS.public_challenges_for_user cs "baz");
-  ignore (
-      CCS.add_challenge cs challenge >|=
-        (fun () ->
-          assert_equal 1 (List.length (CCS.public_challenges_for_user cs "baz"))
-        )
-    );
-  ()
-
-let suite =
-  "challenges suite" >:::
-    [
-      "create challenge" >:: create_challenge
-    ]
-
-let () =
-  run_test_tt_main suite
