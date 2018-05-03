6479f58952a1c9cc8f6cbaaa8bd2bff4b695e845
Bruce Ricard <bruce.ricard@gmail.com>
2018-05-03 00:57:51 +0000

    set pgocaml env variables before building

--- run_tests.sh
+++ run_tests.sh
@@ -9,6 +9,7 @@ sudo -u postgres createuser 'gobblet_server_user'
 cd ~/gobblet/src && psql gobblet -U gobblet_server_user < dao/initdb.sql
 
 eval `opam config env`
+source dao/before_build.sh
 make
 make test
 make install
