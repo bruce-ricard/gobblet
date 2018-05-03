977aec0d77dec85db602acc7c060f90436f4c071
Bruce Ricard <bruce.ricard@gmail.com>
2018-05-03 04:18:18 +0000

    Use . instead of "source" for sh

--- run_tests.sh
+++ run_tests.sh
@@ -8,7 +8,7 @@ sudo -u postgres createdb gobblet
 cd ~/gobblet/src && psql gobblet -U postgres < dao/initdb.sql
 
 eval `opam config env`
-source ~/gobblet/src/dao/before_build.sh
+. ~/gobblet/src/dao/before_build.sh
 export PGUSER='postgres'
 make
 make test
