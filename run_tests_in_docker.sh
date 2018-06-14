2f1cea499fda92d068a651991c0bab1ad1dd1c11
Bruce Ricard <bruce.ricard@gmail.com>
2018-06-14 21:34:07 +0000

    Create script to run the tests in docker and use it in Travis.
    [#158337972]

---
+++ run_tests_in_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eux
+
+if [[ -n "${1:+x}" ]]; then
+    docker run -e INSERTED_GIT_REPO=true -v "$1":/root/gooblet \
+           -it brucericard/ttt:0.6
+else
+    docker run -e INSERTED_GIT_REPO=false \
+           -it brucericard/ttt:0.6
+fi
