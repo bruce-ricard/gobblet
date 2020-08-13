fd499cd1883da57c7d0451980bea796b974826c9
Bruce Ricard <bruce.ricard@gmail.com>
2020-08-13 14:50:29 -0700

    bump image

--- scripts/run_tests_in_docker.sh
+++ scripts/run_tests_in_docker.sh
@@ -4,8 +4,8 @@ set -eux
 
 if [[ -n "${1:+x}" ]]; then
     docker run -e INSERTED_GIT_REPO=true -v "$1":/root/gooblet \
-           -it brucericard/ttt:0.6
+           -it brucericard/ttt:0.7
 else
     docker run -e INSERTED_GIT_REPO=false \
-           -it brucericard/ttt:0.6
+           -it brucericard/ttt:0.7
 fi
