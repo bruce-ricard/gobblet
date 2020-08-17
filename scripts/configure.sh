ee306ebede081e57df06603fba95ce6c47c2db4d
Bruce Ricard <bruce.ricard@gmail.com>
2020-08-16 17:46:33 -0700

    refactor: reorganize dependency installs

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -10,18 +10,26 @@ opam switch 4.02.3
 eval `opam config env`
 eval `opam config env`
 opam install depext -y
-opam depext conf-gmp.1 -y && opam depext conf-m4.1 -y && opam depext conf-pkg-config.1.0 -y
-opam depext conf-libpcre.1 -y && opam depext conf-openssl.1 -y && opam depext conf-zlib.1 -y && opam depext dbm.1.2 -y
-opam depext sqlite3.4.1.3 -y
-opam install dbm -y
-opam install eliom.5.0.0 -y
-opam install extlib -y
-opam install sha -y
-opam install fmt -y
-opam install logs -y
-opam install core -y
-opam install pgocaml -y
-opam install config-file -y
-opam install alcotest -y
-opam install alcotest-lwt -y
-opam install glicko2 -y
+opam depext \
+	 conf-gmp.1 \
+	 conf-m4.1 \
+	 conf-pkg-config.1.0 \
+	 conf-libpcre.1 \
+	 conf-openssl.1 \
+	 conf-zlib.1 \
+	 dbm.1.2 \
+	 sqlite3.4.1.3 -y
+
+opam install -y \
+	 dbm \
+	 eliom.5.0.0 \
+	 extlib \
+	 sha \
+	 fmt \
+	 logs \
+	 core \
+	 pgocaml \
+	 config-file \
+	 alcotest \
+	 alcotest-lwt \
+	 glicko2
