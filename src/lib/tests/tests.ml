2f544daf72f85351737f2acdec8e563f3c9924d1
Bruce Ricard <bruce.ricard@gmail.com>
2020-09-05 12:38:13 -0700

    refactor: extract functions

--- src/lib/tests/tests.ml
+++ src/lib/tests/tests.ml
@@ -1,27 +1,32 @@
+let sync_suite =
+  List.map
+    (fun (name, speed, test) ->
+      Alcotest_lwt.test_case_sync
+        name
+        speed
+        (fun _ -> test())
+    )
+
+let lwt_suite =
+  List.map
+     (
+       fun (name, speed, test) ->
+       Alcotest_lwt.test_case
+         name
+         speed
+         (fun _ _ -> test())
+     )
+
+
 let () =
   Lwt_main.run
   @@ Alcotest_lwt.run
        "game lib"
        [
-         "board suite",
-         (List.map
-            (fun (name, speed, test) ->
-              Alcotest_lwt.test_case_sync
-                name
-                speed
-                (fun _ -> test())
-            )
-            Game_lib_test.board_suite
+         "board suite", (
+           sync_suite Game_lib_test.board_suite
          );
-         "challenge critical section suite",
-         (List.map
-            (
-              fun (name, speed, test) ->
-              Alcotest_lwt.test_case
-                name
-                speed
-                (fun _ _ -> test())
-            )
-            Challenge_test.challenge_suite
+         "challenge critical section suite", (
+           lwt_suite Challenge_test.challenge_suite
          );
        ]
