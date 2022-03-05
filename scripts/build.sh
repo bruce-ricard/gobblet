704757b82c237789642315a951d4114f928ff257
Bruce Ricard <bruce.ricard@gmail.com>
2022-03-04 22:41:05 -0500

    copy file

---
+++ scripts/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -ex
+
+if [ "${INSERTED_GIT_REPO}" = 'true' ]; then
+    echo 'Using inserted git repo'
+else
+    echo 'Pulling latest master...'
+    cd ~/gobblet && git pull
+fi
+
+HASH=$(cd ~/gobblet && git show | head -1)
+echo "Running tests on $HASH"
+
+cd ~/gobblet/src
+eval `opam env`
+rm -f setup.data
+./configure --enable-tests
+
+service postgresql start
+
+cd ~/gobblet/src
+psql -U postgres < dao/init_db.sql
+psql gobblet -U postgres < dao/init_gobblet_db.sql
+psql gobblet -U postgres < dao/set_users_permissions.sql
+
+eval `opam env`
+. ~/gobblet/src/dao/before_build.sh
+
+make
+make install
+(
+    cd server
+    make all
+    make distclean
+)
+make test
