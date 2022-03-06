a51f755f5abff9eb628cb2f278044a2fcea9589a
Bruce Ricard <bruce.ricard@gmail.com>
2022-03-06 11:07:34 -0500

    feature: start server in Docker

---
+++ scripts/start_server_from_docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+set -o xtrace
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+${SCRIPT_DIR}/full_build_from_clean_state.sh
+
+cd /root/gobblet
+cd src/server
+
+eval `opam env`
+
+make test.byte
---
+++ scripts/start_server_in_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+set -o xtrace
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+docker run \
+       -it \
+       -v ~/bruce/gobblet:/root/gobblet \
+       --publish 8080:8080 \
+       ttt \
+       /root/gobblet/scripts/start_server_from_docker.sh
